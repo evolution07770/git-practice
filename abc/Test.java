f5dd110cdc2c352ccab2bb724f2a12ed25efabf3
evolution07770 <evolution@gmail.com>
2021-08-22 11:54:03 +0900

    初回コミット

---
+++ abc/Test.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class Test{
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+	
+	public void doPri(){
+		System.out.println();
+	}
+}
---
+++ abc/TestOya.java
@@ -0,0 +1,8 @@
+package abc;
+class TestOya {
+	public static void main(String[] args) {
+		System.out.println("おや1");
+		Test test = new Test();
+		test.doPri();
+	}
+}
